a7d99084c8111aed5b34372090d6e4096b6c7791
oykucabuk <cabukoyku@gmail.com>
2025-02-15 01:56:52 -0500

    AS2 P1S4

--- server.js
+++ server.js
@@ -6,19 +6,23 @@ const app = express(); // obtain the "app" object
 
 
 
-const HTTP_PORT = process.env.PORT || 8080; // assign a port
+const HTTP_PORT = process.env.PORT || 1010; // assign a port
 
+app.use(express.static('public'));
 
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/views/index.html'));
+//   });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/index.html'));
+  app.get('/', (req, res) => {
+    res.redirect('/about');
   });
 
-//   app.get('/about', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/views/about.html'));
-//   });
+  app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/about.html'));
+  });
 
 
 // start the server on the port and output a confirmation to the console
-app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
+app.listen(HTTP_PORT, () => console.log(`Express server listening on: ${HTTP_PORT}`));
 
